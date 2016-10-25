37028e20443316aa28188e039bdf922556b1e37e
Belem Zhang <belem.zhang@intel.com>
2016-10-25 14:59:03 +0800

    u

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-var page = require('webpage').create(function (browser) {
-    browser.createPage(page)(function (page) {
-        browser.setProxy('child-prc.intel.com', '913', 'http', null, null, function () {
-            page.open('http://cn.bing.com/', function (status) {
-                console.log("Status: " + status);
-                if (status === "success") {
-                    page.render('oreilly.png');
-                }
-                phantom.exit();
-            });
+
+//sudo apt install phantomjs
+//npm install --save phantom
+//npm install --save phantomjs
+
+var path = require('path');
+var childProcess = require('child_process');
+var phantom = require('phantom');
+
+phantom.create({
+    parameters: {
+        'proxy': 'http://child-prc.intel.com:913'
+    }
+}, function (browser) {
+    browser.createPage(function (page) {
+        page.open('http://cn.bing.com', function (status) {
+            console.log("Status: " + status);
+            if (status === "success") {
+                page.render('example.png');
+            }
+            phantom.exit();
         });
     });
 });
