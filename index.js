dbdcca0ca6f02d956476bff2357f57deb5564444
Belem Zhang <belem.zhang@intel.com>
2016-10-28 13:46:00 +0800

    u

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
-var express = require('express')
-var cheerio = require('cheerio')
-var request = require('superagent')
+let express = require('express')
+let cheerio = require('cheerio')
+let request = require('superagent')
 require('superagent-proxy')(request)
-var proxy = process.env.http_proxy || 'http://child-prc.X.com:913'
+let mkdirp = require('mkdirp')
 
-var baseurl = 'http://www.foxebook.net/'
-var url = 'http://www.foxebook.net/publisher/oreilly-media/'
+let proxy = process.env.http_proxy || 'http://child-prc.X.com:913'
+let baseurl = 'http://www.foxebook.net/'
+let url = 'http://www.foxebook.net/publisher/oreilly-media/'
+let publisher =['oreilly-media', 'apress', 'manning-publications', 'packtpub', 'Wiley', 'wrox', 'addison-wesley-professional']
 
-request
-    .get(process.argv[2] || url)
-    .set('User-Agent', 'Mozilla/5.0 (X11 Ubuntu Linux x86_64 rv:49.0) Gecko/20100101 Firefox/49.0')
-    .set('Accept', 'text/html,application/xhtml+xml,application/xmlq=0.9,image/webp,*/*q=0.8')
-    .set('Host', 'www.foxebook.net')
-    .set('Referer', 'http://www.foxebook.net/')
-    .withCredentials()
-    .proxy(proxy)
-    .end(onresponse)
+function createpublisherdir() {
+    for (var pub of publisher) {
+        console.log(pub)
+        mkdirp('./publisher/' + pub, function (err) {
+            if (err) {
+                console.log(err)
+            }
+            else console.log(pub + ' created under \'publisher\' folder.')
+        })
+    }
+}
+
+function geturllist() {
+    var urllist = []
+    for(var pub of publisher) {
+        for (var i = 1; i < 50; i++) {
+            var page = 'http://www.foxebook.net/publisher/' + pub + '/page/' + i + '/'
+            urllist.push(page)
+        }
+    }
+    return urllist
+}
+
+//console.log(geturllist())
+//onrequest()
+//createpublisherdir()
+
+function onrequest() {
+    request
+        .get(url)
+        .set('User-Agent', setua())
+        .set('Accept', 'text/html,application/xhtml+xml,application/xmlq=0.9,image/webp,*/*q=0.8')
+        .set('Host', 'www.foxebook.net')
+        .set('Referer', 'http://www.foxebook.net/')
+        .withCredentials()
+        .proxy(proxy)
+        .end(onresponse)
+}
 
 function onresponse(err, res) {
     if (err) {
         console.log(err)
     } else {
-        //console.log(res.status)
+        console.log(res.status)
         getbooklist(err, res.text)
     }
 }
@@ -35,19 +66,32 @@ function getbooklist(err, html) {
         var $ = cheerio.load($main.html())
         var items = []
         $('div.row').each(function (idx, elem) {
-            //var $element = $(element)
             var $ = cheerio.load(elem)
-            var bookurlslash = baseurl + $('.col-md-9 a').attr('href')
-            var bookurl = bookurlslash.trim().replace('.net//', '.net/')
-            var bookthumburlh200 = 'http:' + $('.col-md-3 img').attr('src')
-            var bookimgurl = bookthumburlh200.trim().replace('._SL200_', '')
+            var urlslash = baseurl + $('.col-md-9 a').attr('href')
+            var url = urlslash.trim().replace('.net//', '.net/')
+            var thumburl = 'http:' + $('.col-md-3 img').attr('src')
+            var imgurl = thumburl.trim().replace('._SL200_', '')
             items.push({
                 title: $('.col-md-9 a').text(),
-                href: bookurl,
-                humburlh200: bookthumburlh200,
-                imgurl: bookimgurl
+                href: url,
+                humburl: thumburl,
+                imgurl: imgurl
             })
         })
         console.log(items)
     }
+}
+
+function setua(){
+    var num = Math.floor(Math.random()*5)
+    var ualist = [
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36',
+        'Mozilla/5.0 (X11 Ubuntu Linux x86_64 rv:49.0) Gecko/20100101 Firefox/49.0', 
+        'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A'
+    ]
+    var ua = ualist[num]
+    console.log(ua)
+    return ua
 }
