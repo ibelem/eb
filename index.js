b213fbdb68af97ed7c41c7227083d989f980902c
Belem Zhang <belem.zhang@intel.com>
2016-11-01 15:24:36 +0800

    u

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const cheerio = require('cheerio')
 const request = require('superagent')
 require('superagent-proxy')(request)
 const Throttle = require('superagent-throttle')
-const fs = require('fs')
-const path = require('path')
-const async = require('async')
+let mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/ebook')
 
 let proxy = process.env.http_proxy || 'http://child-prc.X.com:913'
 const BASEURL = 'http://www.foxebook.net/'
@@ -20,51 +19,10 @@ for (let i in PUBLISHERLIST) {
 
 let throttle = new Throttle({
     active: true,
-    rate: 4,
+    rate: 5,
     ratePer: 12000,
-    concurrent: 10
+    concurrent: 5
 })
-
-let dirpublisher = path.join(__dirname, 'publisher')
-fs.readdir(dirpublisher, function (err) {
-    if (err) {
-        fs.mkdir(dirpublisher, function (err) {
-            if (err) { console.log(err) }
-            else {
-                console.log(dirpublisher + ' created')
-                //createDirSubPublisher()
-            }
-        })
-    }
-    else {
-        console.log(dirpublisher + ' exists.')
-        //createDirSubPublisher()
-    }
-})
-
-function createDirSubPublisher() {
-    for (let pub of PUBLISHER) {
-        let dirsubpublisher = path.join(__dirname, 'publisher', pub)
-        fs.readdir(dirsubpublisher, function (err) {
-            if (err) {
-                fs.mkdir(dirsubpublisher, function (err) {
-                    if (err) { console.log(err) }
-                    else console.log(dirsubpublisher + ' created.')
-                })
-            }
-            //else console.log(dirsubpublisher + ' exists.')
-        })
-    }
-}
-
-function createJSON(err, path, data) {
-    if (err) { console.log(err) }
-    else {
-        fs.writeFileSync(path, JSON.stringify(data));
-        console.log(path + ' created.')
-    }
-}
-
 
 let num = Math.floor(Math.random() * 5)
 let ualist = [
@@ -76,42 +34,40 @@ let ualist = [
 ]
 let ua = ualist[num]
 
-let urllist = []
 for (let pub of PUBLISHER) {
-    for (let i = 1; i < 49; i++) {
+    let urllist = []
+    for (let i = 1; i < 3; i++) {
         let page = 'http://www.foxebook.net/publisher/' + pub + '/page/' + i + '/'
         urllist.push(page)
     }
-}
 
-for (let url of urllist) {
-    if(url.indexOf('/page/1/') >-1){
-        url = url.replace('page/1/', '')
+    for (let url of urllist) {
+        if (url.indexOf('/page/1/') > -1) {
+            url = url.replace('page/1/', '')
+        }
+        urlforjson = url
+        request
+            .get(url)
+            .set('User-Agent', ua)
+            .set('Accept', 'text/html,application/xhtml+xml,application/xmlq=0.9,image/webp,*/*q=0.8')
+            .set('Host', 'www.foxebook.net')
+            .set('Referer', 'http://www.foxebook.net/')
+            .withCredentials()
+            .proxy(proxy)
+            .use(throttle.plugin())
+            .end((err, res) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log(res.status)
+                    getBookList(err, res.text, pub)
+                }
+            })
     }
-    request
-        .get(url)
-        .set('User-Agent', ua)
-        .set('Accept', 'text/html,application/xhtml+xml,application/xmlq=0.9,image/webp,*/*q=0.8')
-        .set('Host', 'www.foxebook.net')
-        .set('Referer', 'http://www.foxebook.net/')
-        .withCredentials()
-        .proxy(proxy)
-        .use(throttle.plugin())
-        .end((err, res) => {
-            if (err) {
-                console.log(err)
-            } else {
-                console.log(res.status)
-                getBookList(err, res.text, url)
-            }
-        })
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
 
-function getBookList(err, html, url) {
+function getBookList(err, html, pub) {
     if (err) { console.log(err) }
     else {
         var $ = cheerio.load(html)
@@ -119,7 +75,7 @@ function getBookList(err, html, url) {
         var $ = cheerio.load($main.html())
         let authors = []
         let format = []
-        let items = []
+
         $('div.row').each(function (idx, elem) {
             var $ = cheerio.load(elem)
             let urlslash = BASEURL + $('.col-md-9 a').attr('href')
@@ -143,15 +99,14 @@ function getBookList(err, html, url) {
             let publishdate = lastline.match(/\d{2,4}(-\d{1,2}){0,2}/g)[0]
             let page = lastline.replace(publishdate, '').trim().match(/\d{1,6}\spages/g)[0].replace('pages', '').trim()
             let formatlist = lastline.replace(publishdate, '').replace(page, '').replace('pages', '').trim().split(',')
-            
+
             for (let i of formatlist) {
                 if (i.trim()) {
                     format.push(i.trim())
                 }
             }
-            console.log(format)
 
-            items.push({
+            oreillymedia.create({
                 title: title,
                 href: url,
                 humburl: thumburl,
@@ -161,33 +116,46 @@ function getBookList(err, html, url) {
                 publishdate: publishdate,
                 page: page,
                 format: format
+            }, function (err, msg) {
+                if (err) console.log(err)
+                else (publisher + ': ' + 'title' + ' - inserted.')
             })
+
             authors = []
             format = []
         })
-        //console.log(items)
-
-        let pathjson = url.replace(BASEURL, '').replace('publisher/', '').replace(/\//g, '-')
-        pathjson = pathjson + '.json'
-        pathjson = pathjson.replace('-.json','.json')
-        createJSON(err, path.join(__dirname, 'publisher', pathjson), items)
     }
 }
 
-function guid() {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-    }
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-        s4() + '-' + s4() + s4() + s4();
-}
+var eBookSchema = new mongoose.Schema({
+    title: String,
+    href: String,
+    humburl: String,
+    imgurl: String,
+    author: Array,
+    publisher: String,
+    publishdate: Date,
+    page: Number,
+    format: Array,
+    edition: String,
+    language: String,
+    isbn10: Number,
+    isbn13: Number,
+    description: String,
+    tableofcontents: String,
+    downloadurlpdf: String,
+    downloadurlepub: String,
+    size: String,
+    uploaddate: Date,
+    lastupdate: Date
+})
 
-String.prototype.replaceArray = function(find, replace) {
-  var replaceString = this;
-  for (var i = 0; i < find.length; i++) {
-    replaceString = replaceString.replace(find[i], replace[i]);
-  }
-  return replaceString;
+var oreillymedia = mongoose.model('oreillymedia', eBookSchema);
+
+String.prototype.replaceArray = function (find, replace) {
+    var replaceString = this;
+    for (var i = 0; i < find.length; i++) {
+        replaceString = replaceString.replace(find[i], replace[i]);
+    }
+    return replaceString;
 }
